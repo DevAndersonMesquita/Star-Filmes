b60bc1449d534348482e8738c862d199053e1428
Anderson Mesquita <andersonmesquita726@gmail.com>
2026-04-06 17:42:04 -0300

    .]

---
+++ public/script.js
@@ -0,0 +1,45 @@
+async function cadastrar() {
+
+    const email = document.getElementById('email');
+    const senha = document.getElementById('senha');
+    const nome = document.getElementById('nome');
+    const date = document.getElementById('date');
+    const cpf = document.getElementById('cpf');
+    
+    const resposta = await fetch("/api/usuario", {
+        
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email: email.value, senha: senha.value, nome: nome.value, date: date.value, cpf: cpf.value })
+    });
+    
+    const dados = await resposta.json();    
+    if (resposta.ok){
+        window.location.href = "indexcine.html";
+    }else{
+        alert(dados.mensagem);
+    
+    }
+} 
+
+async function entrar() {
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    const resposta = await fetch("/api/login", {
+        method : "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify({email: email , senha: senha})
+    });
+    const dados = await resposta.json()
+    if(resposta.ok){
+        localStorage.setItem("token", dados.token)
+        window.location.href = "indexcine.html"
+    }else{
+        alert(dados.mensagem)
+    }
+}
--- src/models/usuario.js
+++ src/models/usuario.js
@@ -2,6 +2,9 @@ import sequelize from "../database/database.js";
 import { DataTypes } from "sequelize";
 
 const usuario = sequelize.define("Usuario",{
+    cpf:{
+        type: DataTypes.STRING 
+    },
     nome: {
         type: DataTypes.STRING
     },
