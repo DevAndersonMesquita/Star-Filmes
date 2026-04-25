d8ddb1270e242f761f10a89e93acdc36229f5518
Anderson Mesquita da Silva <andersonmesquita726@gmail.com>
2026-04-24 21:10:29 -0300

    Refactor server to use environment variable for port

--- server.js
+++ server.js
@@ -28,9 +28,8 @@ app.post("/api/usuario", async (req , res) =>{
     ])
   })
 
-app.listen(3000, () => {
-  console.log("Servidor rodando em http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 
 app.post("/api/login", (req, res ) =>{ 
 
