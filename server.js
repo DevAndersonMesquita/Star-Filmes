dab0d1d08aa6377c61ccee47643b167e3dfd6ca5
Anderson Mesquita <andersonmesquita726@gmail.com>
2026-04-27 16:16:55 -0300

    servidor corrigido.

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-import express from "express"
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import usuario from "./src/models/usuario.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "indexcine.html"));
+});
 
 app.get("/api/filmes", (req, res) => {
   res.json([
@@ -14,18 +23,18 @@ app.get("/api/filmes", (req, res) => {
   ]);
 });
 
-usuario.sync()
+usuario.sync();
 
 app.post("/api/usuario", async (req, res) => {
-  const { email, senha, nome } = req.body
-  await usuario.create({ email, senha })
-  res.json([{ email }, { senha }])
-})
+  const { email, senha } = req.body;
+  await usuario.create({ email, senha });
+  res.json([{ email }, { senha }]);
+});
 
 app.post("/api/login", (req, res) => {
-  const { email, senha } = req.body
-  console.log(req.body)
-  res.json([{ email }, { senha }])
+  const { email, senha } = req.body;
+  console.log(req.body);
+  res.json([{ email }, { senha }]);
 });
 
 const PORT = process.env.PORT || 3000;
